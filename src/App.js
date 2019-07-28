cd6bb518f873b74c96827a1db2b6a33f4907c8d8
unknown <Rampritsahani@outlook.in>
2019-07-28 16:23:26 +0530

    initial update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import Card from "./components/card/card.component";
+import "./App.css";
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchMonster: ""
+    };
+  }
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(user => this.setState({ monsters: user }));
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <input
+          type="search"
+          onChange={e => this.setState({ searchMonster: e.target.value })}
+          className="input has-text-center w-50"
+          placeholder="Search monster"
+        />
+        <CardList>
+          {this.state.monsters.map(monster => (
+            <Card key={monster.name} monster={monster} />
+          ))}
+        </CardList>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
